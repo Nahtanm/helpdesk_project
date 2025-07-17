ac5b30ffaf5cd97214d1177642c431cbaf40e867
Nahtanm <nahtanmcs0019@gmail.com>
2025-07-16 22:06:45 -0300

    Endpoint create tecnico

--- src/main/java/com/dev_santos/helpdesk/resources/TecnicoResources.java
+++ src/main/java/com/dev_santos/helpdesk/resources/TecnicoResources.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +37,11 @@ public class TecnicoResources {
 				.map(x -> new TecnicoDTO(x))
 				.collect(Collectors.toList());
 		return ResponseEntity.status(HttpStatus.OK).body(tecnicosList);
+	}
+	
+	@PostMapping
+	public ResponseEntity<TecnicoDTO> create(@RequestBody Tecnico tecnico){
+		return ResponseEntity.status(HttpStatus.CREATED).body(new TecnicoDTO(services.create(tecnico)));
 	}
 	
 } 
--- src/main/java/com/dev_santos/helpdesk/services/TecnicoServices.java
+++ src/main/java/com/dev_santos/helpdesk/services/TecnicoServices.java
@@ -3,10 +3,12 @@ package com.dev_santos.helpdesk.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.dev_santos.helpdesk.domain.Tecnico;
 import com.dev_santos.helpdesk.repositories.TecnicoRepository;
+import com.dev_santos.helpdesk.services.exceptions.ExistingObjectException;
 import com.dev_santos.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,6 +23,18 @@ public class TecnicoServices {
 	
 	public List<Tecnico> findAll(){
 		return repository.findAll();
+	}
+	
+	public Tecnico create(Tecnico tecnico) {
+		
+		try {
+			return repository.save(tecnico);
+		}catch(DataIntegrityViolationException e) {
+			throw new ExistingObjectException("Objeto existente");
+		}catch(IllegalArgumentException e) {
+			throw new ExistingObjectException("Objeto existente");
+		}
+		
 	}
 	
 }
